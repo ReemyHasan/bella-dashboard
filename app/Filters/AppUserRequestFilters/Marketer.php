9fcfb524c0a455d4a3ecb9f25cd185e69ab5c913
reem.hasan.831 <reema.samer.815@gmail.com>
2026-08-19 22:40:15 +0300

    add filters

---
+++ app/Filters/AppUserRequestFilters/Marketer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filters\AppUserRequestFilters;
+
+use App\Filters\QueryFilter;
+use App\Filters\FilterContract;
+
+class Marketer extends QueryFilter implements FilterContract
+{
+    public function handle($value): void
+    {
+        $this->query->where(
+            'app_user_id',
+            $value
+        );
+    }
+
+    public function handleRange($value): void
+    {
+        // Not applicable
+    }
+}
---
+++ app/Filters/FinancialAdjustmentFilters/Marketer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filters\FinancialAdjustmentFilters;
+
+use App\Filters\QueryFilter;
+use App\Filters\FilterContract;
+use App\Models\AppUser;
+
+class Marketer extends QueryFilter implements FilterContract
+{
+    public function handle($value): void
+    {
+        $this->query->where('requested_for_type', AppUser::class)->where(
+            'requested_for_id',
+            $value
+        );
+    }
+
+    public function handleRange($value): void
+    {
+        // Not applicable
+    }
+}
